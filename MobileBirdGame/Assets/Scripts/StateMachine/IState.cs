0b210bde79211de2f93e9e4c4f2cb0d448a3e79f
benviss <ben.vissotzky@gmail.com>
2018-10-23 20:56:15 -0700

    State Machine state switching, enemy sprite/mat

---
+++ MobileBirdGame/Assets/Scripts/StateMachine/IState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IState {
+
+  void Enter();
+
+  void Execute();
+
+  void Exit();
+    
+}
---
+++ MobileBirdGame/Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour {
+
+  private IState currentlyRunningState;
+  private IState previousState;
+
+  public void ChangeState(IState newState)
+  {
+    if (currentlyRunningState != null) currentlyRunningState.Exit();
+    
+    previousState = currentlyRunningState;
+
+    currentlyRunningState = newState;
+    currentlyRunningState.Enter();
+  }
+
+  public void ExecuteStateUpdate()
+  {
+    var runningState = currentlyRunningState;
+    if (runningState != null) runningState.Execute();
+  }
+
+  public void SwitchToPreviousState()
+  {
+    currentlyRunningState.Exit();
+    currentlyRunningState = previousState;
+    currentlyRunningState.Enter();
+  }
+}
