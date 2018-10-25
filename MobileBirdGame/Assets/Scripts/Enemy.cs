055b099c58a918a278154ce2783f52396b827701
Ben vissotzky <ben.vissotzky@gmail.com>
2018-10-24 17:05:32 -0700

    Implements spawn manager for units

--- MobileBirdGame/Assets/Scripts/Enemy.cs
+++ MobileBirdGame/Assets/Scripts/Enemy.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-  public float movementSpeed;
-  private StateMachine stateMachine = new StateMachine();
-	// Use this for initialization
-	void Start () {
-    this.stateMachine.ChangeState(new Roam(this.gameObject, movementSpeed, 10, -10));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    this.stateMachine.ExecuteStateUpdate();
-	}
+    public float movementSpeed;
+    private StateMachine stateMachine = new StateMachine();
+    // Use this for initialization
+    void Start()
+    {
+        this.stateMachine.ChangeState(new Roam(this.gameObject, movementSpeed, 10, -10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.stateMachine.ExecuteStateUpdate();
+    }
 }
---
+++ MobileBirdGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public List<GameObject> spawnPoints = new List<GameObject>();
+    private List<GameObject> spawnedUnits = new List<GameObject>();
+
+    public GameObject spawnPrefab;
+    public float maxSpawned = 0;
+
+    public float maxSpawnRate;
+    private float nextSpawnTime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //I think this is unsafe
+        foreach (var spawnedUnit in spawnedUnits) {
+            if (spawnedUnit == null) {
+                spawnedUnits.Remove(spawnedUnit);
+            }
+        }
+        //check if alive enemies < max enemies && next spawn time available
+        if (spawnedUnits.Count < maxSpawned && nextSpawnTime < Time.time) {
+            SpawnUnit();
+            nextSpawnTime = Time.time + Random.Range(0,maxSpawnRate);
+        }
+	}
+
+    void SpawnUnit()
+    {
+        //for spawned unit on ground, they will most likely need to get raised up by half of their height
+        int selectedSpawnPoint = Random.Range(0, spawnPoints.Count);
+        GameObject newSpawnedUnit = Instantiate(spawnPrefab, spawnPoints[selectedSpawnPoint].transform.position, this.transform.rotation, this.transform);
+        spawnedUnits.Add(newSpawnedUnit);
+    }
+}
